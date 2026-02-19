6da16656d4ba02b94e87b00f9515ff5bbcd04a03
UncreativePenquin <uncreativepenquin@gmail.com>
2026-02-19 12:00:49 -0500

    Added collision to floor, and temp-player movement
    the collision is only applied mainly to the floor used right now,
    the player movement uses the arrow keys for us to move around in the enviorment

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,4 +1,24 @@
 extends CharacterBody2D
 
+var speed = 150 ## this is test movement code to experiment with, not the users movement code 
+var movement = Vector2.ZERO
+var gravity = 9.8
 func _ready() -> void:
-    pass
+	pass
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"): #moves right and left
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
+	
+	if is_on_floor(): #applies gravity if not on floor (y up is negative(
+		velocity.y = 0
+	else:
+		velocity.y += gravity
+	
+	if Input.is_action_pressed("ui_up") and is_on_floor(): #jump code
+		velocity.y = -speed * 2
+	move_and_slide()
